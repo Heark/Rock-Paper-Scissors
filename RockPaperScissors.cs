6a1372f9157b5e25a355b4d61b0696510a2bf4c6
Heark <antwauntune25@wcpsstudent.org>
2016-05-23 14:29:04 -0400

    start user input setup

--- RockPaperScissors.cs
+++ RockPaperScissors.cs
@@ -10,7 +10,19 @@ namespace RockPaperScissors
     {
         static void Main(string[] args)
         {
-
+        Console.WriteLine("Rock, Paper, or Scissors?");
+        string user_choice = Console.ReadLine();
+        
+        if(user_choice == "Rock")
+        {
+            string comp_choice = getCompChoice();
+            if (comp_choice == "Rock" )
+            {
+                ret_msg = "You both chose Rock! Tie Game!"
+            }
+        }
+        Console.WriteLine(ret_msg);
+        Console.ReadLine();
         }
     }
 }
